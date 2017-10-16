f504420f6267767631125b5fe40acccf4524614b
Kevin Moore <Kevin Moore>
2017-10-16 15:29:55 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,15 @@
+/*
+Kevin Moore 3907616
+Zachary Mell 3855374
+Exercise 1
+MW 3-415
+*/
+
+public class BasicGit{
+	public static void main(String args[]){
+		int i = 0;
+		for(i = 0; i <= 10; i++){
+			System.out.println(i);
+		}
+	}
+}
