e843ebebbd0d41629afd4dbff23194e499249ae7
Kevin Moore <Kevin Moore>
2017-10-16 15:37:52 -0400

    final

--- BasicGit.java
+++ BasicGit.java
@@ -7,8 +7,8 @@ MW 3-415
 
 public class BasicGit{
 	public static void main(String args[]){
-		int i = 0;
-		for(i = 0; i <= 10; i++){
+		int i = 1;
+		for(i = 1; i <= 10; i++){
 			System.out.println(i);
 		}
 	}
